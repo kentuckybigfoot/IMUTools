4d216a58899d1499dbdc7b2ffca1dc41c60e0ac0
kentuckybigfoot <kentuckybigfoot@gmail.com>
2016-04-07 16:21:59 -0500

    - Initial commit

---
+++ convertMtoA.m
@@ -0,0 +1,18 @@
+function [ alpha beta gamma ] = convertMto( R )
+%CONVERTMTOA Convert 3D rotation matrix to rotation angles
+%   Input full horizontal rotation matrix, R, and return alpha (yaw CC pos
+%   x axis), beta (pitch CC pos y axis), abd gamma (roll CC pos x axis).
+%   
+%   http://planning.cs.uiuc.edu/node103.html used as reference.
+
+%Yaw (alpha) (CC Z Axis)
+alpha = atan2(R(4), R(1));
+
+%Pitch (beta) (CC Y Axis)
+beta = atan2(-R(7),sqrt(R(8)^2 + R(9)^2));
+
+%Roll (gamma) (CC X Axis)
+gamma = atan2(R(8),R(9));
+
+end
+
